ff7bd68d401423ac88b27d613d76c8c88093f037
Scott Abbott <scott.abbott.a@gmail.com>
2018-04-26 15:45:19 -0600

    added title

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -4,6 +4,7 @@ import splash from '../assets/images/landing/Head.jpg';
 export default props =>{
     return(
         <div className='landing-img'id="landing">
+        <h1 style={{position: 'absolute', color:'white', top: '100px'}}>PORTFOLIO</h1>
         <img src={splash} alt="landing imd"/>
         </div>
     );
