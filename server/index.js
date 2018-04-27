92c2e20d0a95bee2ebf3e6cbc00a410887ff57e8
Scott Abbott <scott.abbott.a@gmail.com>
2018-04-26 22:37:05 -0600

    added OAuth2

--- server/index.js
+++ server/index.js
@@ -6,7 +6,16 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 // const config = require("./config");
 
-const {USER, PASS, SERVER_PORT} = process.env;
+const {
+  USER, 
+  PASS,
+  SERVER_PORT,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REFRESH_TOKEN,
+  ACCESS_TOKEN,
+  MY_EMAIL
+} = process.env;
 
 const app = express()
 app.use(cors());
@@ -18,9 +27,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    // type: 'OAuth2',
+    type: 'OAuth2',
     user: USER,
-    pass: PASS
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    refreshToken: REFRESH_TOKEN,
+    accessToken: ACCESS_TOKEN,
+    expires: 3600,
   }
 });
 
@@ -30,7 +43,7 @@ app.post('/api/contact', (req, res) => {
 
   const mailOptions = {
     from: email,         // Sender of the email
-    to: 'scott.abbott.a@gmail.com',  // Recipient of the email
+    to: MY_EMAIL,  // Recipient of the email
     subject: `${email} - ${subject}`,              // Subject of the email
     text: `${name} - ${message}`,                // Message of the email
 
